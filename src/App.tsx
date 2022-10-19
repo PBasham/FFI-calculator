43d62d8da19627d028f1ba2587af5eb20bf795f2
patrickbasham <PatrickTBasham@gmail.com>
2022-10-19 16:13:25 -0400

    initial commit and changed some files

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+/*========================================
+        Import Dependencies
+========================================*/
+import React from 'react';
+// logo --------------------------------------------------
+// import logo from './logo.svg';
+// styling --------------------------------------------------
+import './styles.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+/*========================================
+        Import Dependencies
+========================================*/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// components --------------------------------------------------
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
